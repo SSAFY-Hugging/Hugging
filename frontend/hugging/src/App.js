bb06ee0f1946b71c215d74c4ba0b41e31adcb0ba
김호진 <hjay1029@naver.com>
2022-09-26 08:55:08 +0900

    feat: counselor/user login

--- frontend/hugging/src/App.js
+++ frontend/hugging/src/App.js
@@ -1,9 +1,39 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
+import { Routes, Route, Link } from 'react-router-dom'
+import UserLogin from './user/UserLogin.js'
+import CounselorLogin from './counselor/CounselorLogin';
+import RegisterCategory from './user/RegisterCategory';
+import RegisterProfile from './user/RegisterProfile';
+import Navbar from './components/NavBar';
+import Nav from './components/Nav'
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars } from "@fortawesome/free-solid-svg-icons";
+import MyCalendar from './components/MyCalendar';
+import Location from './components/Location';
+import RedirectUri from './user/RedirectUri';
+// import scrollbar from 'smooth-scrollbar';
+
+// // smooth scroll 설정
+// scrollbar.init(document.querySelector('#smooth-scroll'));
 
 function App() {
   return (
     <div className="App">
+      <Navbar/>
+      <Nav/>
+       {/* <Location></Location> */}
+      <Routes>
+        {/* <MyCalendar></MyCalendar> */}
+        <Route path="/redirecturi" element={<RedirectUri/>}></Route>
+        <Route path="/location" element={<Location/>}></Route>
+        <Route path="/calendar" element={<MyCalendar/>}></Route>
+        <Route path="/login" element={<UserLogin/>} />
+        <Route path="/counselor/login" element={<CounselorLogin/>} />
+        <Route path="/category" element={<RegisterCategory/>} />
+        <Route path="/profile" element={<RegisterProfile/>} />
+        <Route path="/profile_img" element={ <></>}></Route>
+      </Routes>
     </div>
   );
 }
--- frontend/hugging/src/store.js
+++ frontend/hugging/src/store.js
@@ -1,5 +1,65 @@
-import { configureStore } from '@reduxjs/toolkit'
+import { configureStore, createSlice } from '@reduxjs/toolkit'
+
+let counselor = createSlice({
+    name: 'counselor',
+    initialState: {
+      id: '',
+      pw: '',
+    },
+    reducers: {
+      loginCounselor: (state, action)=> {       
+          state.name = action.payload.name;
+          state.id = action.payload.id;
+          state.isLoggined = true;
+
+      },
+      clearCounselor: (state) => {
+        state.name = "";
+        state.id = "";
+        state.isLogin = false;
+      },
+    }
+})
+
+let user = createSlice({
+    name: 'user',
+    initialState: {
+      "email": "",
+      "name": "",
+      "nickname": "",
+      "age": "",
+      "gender": "",
+      "emotion" : {}
+   },
+    reducers: {
+      changeUser(state, action) {
+        state.nickname = action.payload.nickname;
+        state.age = action.payload.age;
+        state.gender = action.payload.gender
+      },
+      changeEmotion(state, action) {
+        state.emotion = action.payload
+      },
+      loginUser: (state, action)=> {       
+          state.name = action.payload.name;
+          state.email = action.payload.email;
+          state.isLoggined = true;
+
+      },
+      clearUser: (state) => {
+        state.name = "";
+        state.id = "";
+        state.isLogin = false;
+      },
+    }
+})
+
+export let { loginUser, changeEmotion, changeUser } = user.actions
+export let { loginCounselor } = counselor.actions
 
 export default configureStore({
-  reducer: { }
+  reducer: { 
+    counselor: counselor.reducer,
+    user: user.reducer,
+  }
 }) 
